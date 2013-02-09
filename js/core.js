b44a876b75c2e9ab67379ec8f91816a29c780d62
Phil Corbett <corbett.phil@gmail.com>
2013-02-09 07:01:45 -0500

    add interface tie-ins

--- js/core.js
+++ js/core.js
@@ -1,3 +1,32 @@
 $(function () {
+	function reload() {
+		$.ajax({
+			url: 'js/data.js',
+			success: function () {
+				$('[data-call=name]').text('Names loaded');
+			}
+		});
+	}
 	
+	reload();
+	
+	$('[data-call=call]').on('click', function () {
+		var s = Call.Next();
+		if(s) {
+			$('[data-call=name]').text(s.name);
+		} else {
+			$('[data-call=name]').text('Skipped All Students');
+		}
+		
+	});
+	
+	$('[data-call=skip]').on('click', function () {
+		var s = Call.Skip();
+		$('[data-call=name]').text('Skipped ' + s.name);
+	});
+	
+	$('[data-call=reload]').on('click', function () {
+		Call.Clear();
+		reload();
+	});
 });
